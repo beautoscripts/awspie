64b08384bbf35b266a1085fc6b50fc1e18fed4cc
funix <beeautoscript@gmail.com>
2022-01-09 12:45:42 +0530

    main launcher

--- main.py
+++ main.py
@@ -1,31 +1,9 @@
 #!/usr/bin/env python3
 
 import os
+import shutil
 import sys
-
-
-# function : message handler
-def messenger(msg: str)-> str:
-    # ANSI color code
-    RED = "\033[1;31m"
-    YELLOW = "\033[1;33m"
-    BLUE = "\033[1;34m"
-    GREEN = "\033[1;32m"
-    CLOSE = "\033[0m"
-
-
-    if msg == "invalid_args":
-        error_msg = f"""
-{RED}| Error:{CLOSE} Invalid arguments
-{BLUE}| Info:{CLOSE} Try running awspie -help
-        """
-        return error_msg
-    if msg == "invalid_awspieconfig":
-        error_msg = f"""
-{RED}| Error:{CLOSE} AWSPIE setup configuration missing
-{BLUE}| Info:{CLOSE} Run awspie setup
-        """
-        return error_msg
+from messenger.messages import Messengers
 
 # function: validate the awspie configuration
 def validate_awspie_config() -> bool:
@@ -41,8 +19,29 @@ def validate_awspie_config() -> bool:
     else:
         return False
 
+
+# function: validate aruguments
+def validate_args(arglist: list) -> bool:
+    
+    # dict: parent args (key): length of argument (value)
+    args_dict = {
+        '-help':1,
+        '-setup':1,
+        '-version':1
+    }
+
+    # check parent args availability in dict
+    if arglist[0] in args_dict and len(arglist) == args_dict.get(arglist[0]):
+        return True
+    else:
+        return False    
+
+
 # function: main
 def main():
+    # obj: Messenger
+    msg_obj = Messengers()
+
     """
         - check if the valid arguments are passed
         - check if awspie configuration files are available
@@ -50,11 +49,34 @@ def main():
     if len(sys.argv[1:]) == 0:
         # validating awspie configuration and arguments
         if not validate_awspie_config():
-            print(messenger("invalid_awspieconfig"))
+            msg_obj.messenger_run("invalid_awspieconfig")
             quit(1)
         else:
-            print(messenger("invalid_args"))
+            msg_obj.messenger_run("invalid_args")
             quit(1)
+
+    # check for valid parent arguments
+    args_list = sys.argv[1:]
+    if not validate_args(args_list):
+        print("ERROR")
+        quit(1)
+    else:
+        if args_list[0] == '-help':
+            print(msg_obj.messenger_run("run_help"))
+            quit(0)
+        if args_list[0] == '-version':
+            print(msg_obj.messenger_run("run_version"))
+            quit(0)
+
+        if args_list[0] == '-setup':
+            profile_dir_path = "/usr/local/awspie"
+            if os.path.isdir(profile_dir_path):
+                try:
+                    shutil.rmtree(profile_dir_path)
+                    quit(0)
+                except PermissionError:
+                    print(msg_obj.messenger_run("permission_error"))
+                    quit(1)
 
 # Start main
 if __name__ == "__main__":
